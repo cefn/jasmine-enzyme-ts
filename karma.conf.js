90030273291f500c785125f8759e45fc69c58189
Cefn Hoile <github.com@cefn.com>
2021-07-03 16:16:36 +0100

    Update and simplify - two tests now pass

--- karma.conf.js
+++ karma.conf.js
@@ -1,32 +1,35 @@
 // Karma configuration
 // Generated on Thu Mar 08 2018 08:55:11 GMT-0800 (PST)
-var webpackConfig = require('./webpack.config.js');
+var webpackConfig = require("./webpack.config.js");
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
-    basePath: '',
-    mode: 'development',
-    frameworks: ['jasmine'],
-    files: [
-      'src/*.spec.tsx'
+    basePath: "",
+    mode: "development",
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-webpack"),
+      require("karma-chrome-launcher"),
+      require("karma-spec-reporter"),
     ],
+    frameworks: ["jasmine"],
+    files: ["src/*.spec.tsx"],
     preprocessors: {
-      'src/*.spec.tsx': ['webpack'],
+      "src/*.spec.tsx": ["webpack"],
     },
     webpack: {
-      stats: "verbose",
       mode: "development",
-      devtool: "cheap-module-eval-source-map",
+      devtool: "source-map",
       module: webpackConfig.module,
       resolve: webpackConfig.resolve,
     },
-    reporters: ['spec'],
+    reporters: ["spec"],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_DEBUG,
+    // logLevel: config.LOG_DEBUG,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ["Chrome"],
     singleRun: true,
-    concurrency: Infinity
-  })
-}
+    concurrency: Infinity,
+  });
+};
--- src/index.spec.tsx
+++ src/index.spec.tsx
@@ -1,30 +1,20 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import * as TestUtils from "react-dom/test-utils";
 import * as Enzyme from "enzyme";
 // See the note in the tsconfig.json file about this import
 // import * as Adapter from "enzyme-adapter-react-16";
-import Adapter = require("enzyme-adapter-react-16");
+import Adapter = require("@wojtekmaj/enzyme-adapter-react-17");
 import jasmineEnzyme from "jasmine-enzyme";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-
-class Hello extends React.Component<undefined, undefined> {
-  render(): JSX.Element {
-    return (<div>hello</div>);
-  }
+function Hello(){
+  return (<div>hello</div>);
 }
 
 describe('Jasmine/Enzyme Sanity Tests', () => {
   beforeEach(() => {
     jasmineEnzyme();
-  });
-
-  it('says hello via renderIntoDocument', () => {
-    const hello = TestUtils.renderIntoDocument(<Hello/>) as Hello;
-    const el = ReactDOM.findDOMNode(hello) as HTMLElement;
-    expect(el.innerText).toEqual("hello");
   });
 
   it('says hello via mount', () => {
